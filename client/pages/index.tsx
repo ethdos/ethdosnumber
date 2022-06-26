c5d4937d2095fcb63dddbd58b112e900864b63c3
Sampriti Panda <sampritipanda@outlook.com>
2022-06-26 07:54:15 -0400

    Change FAQ

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Title } from "../components/Base";
+import { StepperHover, Button, Title } from "../components/Base";
 
 const Home: NextPage = () => {
   return (
@@ -21,50 +21,55 @@ const Home: NextPage = () => {
               <div className="mb-8">
                 <strong>What is this site?</strong>
                 <div>
-                  <a href="https://heyanon.xyz">heyanon </a>
-                  is a way for people who did cool stuff on Ethereum to
-                  broadcast messages anonymously on Twitter. Historic moment
-                  feeds are curated by the
-                  <a href="https://twitter.com/heyanonxyz"> @heyanonxyz </a>
-                  account, such as the{" "}
-                  <a href="https://twitter.com/DAOHackGossip">DAO hack</a>
-                  feed. Anyone whose participation in the moment can be verified
-                  on-chain can post to the feed. The magic is that you don’t
-                  need to reveal your address when you do.
+                  <a href="https://ethdos.xyz">ETHdos </a>
+                  is an analogue to the Bacon Number for Ethereum.
+                  broadcast messages anonymously on Twitter. Using recursive
+                  zk-SNARKS, find the degrees of separation between Vitalik and you
+                  through your friends without leaking any information.
                   <br />
                   <em>Not even to the site admins.</em>
                 </div>
               </div>
 
               <div className="mb-8">
-                <strong>How does it work?</strong>
+                <strong>How to use?</strong>
                 <div>
-                  When you send a message with heyanon, you generate a
-                  <a href="https://en.wikipedia.org/wiki/Zero-knowledge_proof">
-                    {" "}
-                    zero-knowledge proof{" "}
+                  <a href="/send/paste">
+                    <StepperHover>ETHdos number</StepperHover>
                   </a>
-                  that you were involved with a certain event on-chain. This
-                  proof hides all information about your address. The proof is
-                  all that is sent to the{" "}
-                  <a href="https://heyanon.xyz">heyanon</a> backend for
-                  verification. Upon verification, the proof is posted to
-                  <a href="https://ipfs.io/">ipfs</a>
-                  and your message is sent via the specified event feed bot. The
-                  message has with it a verify link twitter readers can use to
-                  the proof located on ipfs themselves. For more, check out our
-                  github (TODO: add github).
+                  <br/>
+                  <a href="/share/QmZwz1zBDWSpU5vEJhanb2LixjxVaL4snXDVnFrX55ntgR">
+                    <Button>Example Proof</Button>
+                  </a>
                 </div>
               </div>
 
               <div className="mb-8">
-                <strong>How can you contribute or follow along?</strong>
+                <strong>How does it work?</strong>
                 <div>
-                  Follow{" "}
-                  <a href="https://twitter.com/heyanonxyz">@heyanonxyz</a> for
-                  updates and new message feeds. Join our{" "}
-                  <a href="https://discord.gg/4J3XzZJf">discord</a> for misc.
-                  discussion.
+                This project uses
+                <a href="https://z.cash/technology/zksnarks/">
+                  {" "}recursive zk-snarks{" "}</a>
+                to make composable proofs between different people. Vitalik is degree 0
+                and he can use cryptography-magic to send signatures to his 
+                close friends which allow them to prove that they are degree 1
+                away from Vitalik. 
+                <br/><br/>
+                Those degree-1 friends can send cryptographic proofs to their friends
+                and they can claim that they are degree-2 and so on. The craziest thing
+                is that since each propagation happens through zk, the path from
+                degree-0 to degree-k is hidden. This allows for a fun collector game
+                without having privacy concerns.
+                <br/><br/>
+                Users can also post these proves on chain to mint NFTs claiming that
+                they are degree-k close to Vitalik. Who wouldn't want an NFT saying that
+                you are almost besties with Vitalik :P
+                <br/><br/>
+                This allows for a fun social experiment where users would try to
+                have the lowest degree of separation by talking to their friends!
+                <br/><br/>
+                For more, check out our github:
+                <a href="https://github.com/nalinbhardwaj/ethdosnumber">{" "}github.com/nalinbhardwaj/ethdosnumber</a>
                 </div>
               </div>
             </div>
